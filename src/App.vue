33a617359f80ed52ee6edd358e9caea3c8a95b8b
Josué Camelo <cloudone@josuecamelo.com>
2023-01-06 17:34:17 -0300

    Componente Placar

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,92 @@
 <template>
+  <div>
+    <ScoreBoard />
+    <template v-if="this.question">
+      <h1 v-html="this.question">
+      </h1>
 
-  <section class="score">
-    Jogador <span>{{ this.win_count }}</span> x 
-   <span>{{ this.lose_count }}</span> Computador
-  </section>
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input 
+          type="radio" 
+          name="options" 
+          :disabled="this.answerSubmitted"
+          :value="answer" 
+          v-model="this.chosenAnswer">
 
-  <template v-if="this.question">
-    <h1 v-html="this.question">
-    </h1>
+        <label v-html="answer"></label><br>
+      </template>
+      <button class="send" type="button" v-if="!this.answerSubmitted" name="button" @click="this.submitAnswer()">Send</button>
 
-    <template v-for="(answer, index) in this.answers" v-bind:key="index">
-      <input 
-        :disabled="this.answerSubmitted" 
-        type="radio" name="options" 
-        :value="answer" 
-        v-model="this.chosen_answer">
+      <section v-if="this.answerSubmitted" class="result">
+        <h4 
+          v-if="this.chosenAnswer == this.correctAnswer"
+          v-html="'&#9989; Parabéns! A resposta ' + this.correctAnswer + ' está correta!'">
+        </h4>
+
+        <h4 
+          v-else 
+          v-html="'&#10060; Que pena, a resposta está errada. A resposta correta é ' + this.correctAnswer + '.'"
+          ></h4>
         
-      <label v-html="answer"></label><br>
-    </template>
+        <button class="send" type="button" @click="getNewQuestion()">Next question</button>
+      </section>
+    </template>   
 
-    <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Confirmar</button>
-
-  </template>
-  
-  <section class="result" v-if="this.answerSubmitted">
-    <template v-if="this.chosen_answer == this.correctAnswer">
-      <h4>&#9989; Parabéns, a resposta "{{this.correctAnswer}}" está correta.</h4>
-    </template>
-    <template v-else>
-      <h4>&#10060;  Que pena, a resposta está errada. A resposta correta é "{{this.correctAnswer}}".</h4>
-    </template>
-    <button @click="this.getNewQuestion()" class="send" type="button">Próxima pergunta</button>
-  </section>
-
+  </div>
 </template>
 
 <script>
+import ScoreBoard from "./components/ScoreBoard.vue";
 export default {
   name: 'App',
-  components:[],
-  data(){
+  components: {
+    ScoreBoard
+  },
+  data() {
     return {
       question: undefined,
-      incorrectAnswer: undefined,
+      incorrectAnswers: [],
       correctAnswer: undefined,
-      chosen_answer: undefined,
+      chosenAnswer: undefined,
       answerSubmitted: false,
     }
   },  
   computed: {
-    answers(){
-      let answers = JSON.parse(JSON.stringify(this.incorrectAnswer));
-      answers.splice(Math.round( Math.random() * answers.length),0,this.correctAnswer);
-      return answers;
+    answers() {
+      let answers = JSON.parse(JSON.stringify(this.incorrectAnswers))
+      answers.splice( Math.round(Math.random() * answers.length) , 0, this.correctAnswer)
+      return answers
     }
   },
-  created(){
-    this.getNewQuestion()
-  },
-  methods:{
-    submitAnswer(){
-      if(!this.chosen_answer){
-        alert('Pick one of the option');
-      }else{
-        this.answerSubmitted = true;
-        if(this.chosen_answer == this.correctAnswer){
-          console.log('acertou');
-        }else{
-          console.log('errou')
-        }
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert('Pick one of the options')
+        return
+      }
+      this.answerSubmitted = true
+      if (this.chosenAnswer == this.correctAnswer) {
+        //
+      } else {
+        this.loseCount++
+        //
       }
     },
-
-    getNewQuestion(){
-      this.answerSubmitted = false;
-      this.chosen_answer = undefined;
-      this.question = undefined;
-
-      this.axios.get('https://opentdb.com/api.php?amount=1&category=18')
-      .then((response) => {
-        this.question = response.data.results[0].question;
-        this.incorrectAnswer = response.data.results[0].incorrect_answers;
-        this.correctAnswer = response.data.results[0].correct_answer;
-      })
+    getNewQuestion() {
+      this.answerSubmitted = false
+      this.chosenAnswer = undefined
+      this.question = undefined
+      this.axios
+        .get('https://opentdb.com/api.php?amount=1&category=18&difficulty=medium')
+        .then(response => {
+          this.question = response.data.results[0].question
+          this.incorrectAnswers = response.data.results[0].incorrect_answers
+          this.correctAnswer = response.data.results[0].correct_answer
+        })
     }
+  },
+  created() {
+    this.getNewQuestion()
   }
 }
 </script>
@@ -97,37 +100,18 @@ export default {
   color: #2c3e50;
   margin: 60px auto;
   max-width: 960px;
-
-}
-
-h1 {
-  margin-top: 40px;
-}
-
-input[type='radio']{
-  margin: 12px 4px;
-}
-
-button.send {
-  margin-top: 12px;
-  height: 40px;
-  min-width: 120px;
-  padding: 0 16px;
-  color: #fff;
-  background-color: #1867c0;
-  border: 1px solid #1867c0;
-  cursor: pointer;
-}
-
-section.score {
-  border-bottom: 1px solid black;
-  padding: 24px;
-  font-size: 18px;
-
-  span {
-    padding: 8px;
-    font-weight: bold;
-    border: 1px solid black;
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
   }
 }
 </style>
